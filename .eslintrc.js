fb9c5a9c49b39ee762766daa104cd7dc18229965
wanghaoran <309568486@qq.com>
2026-08-18 02:03:18 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  rules: {
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'indent': 'off',
+    // "trailing-comma": 'off',
+    indent: ['error', 2],
+    'vue/require-v-for-key': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-inferrable-types': 0,
+    'vue/no-use-v-if-with-v-for': 0,
+    'vue/no-reserved-component-names': 0,
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  globals: {
+    defineEmits: "readonly",
+    defineProps: "readonly",
+  }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+// eslint-disable-next-line
+// @ts-ignore
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -7,18 +9,10 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
